aeb19de94953ab17b94d43769ac66ab4da619686
chxry <chxry@flopper.me>
2023-06-08 22:06:09 +0000

    hypertext transfer protocol is secured

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::fs;
+use std::fs::{File, self};
 use std::net::SocketAddr;
 use std::collections::HashMap;
+use std::sync::Arc;
+use std::io::BufReader;
 use tokio::task;
 use tokio::net::{TcpListener, TcpStream};
 use hyper::header;
 use hyper::server::conn::http1 as server;
 use hyper::client::conn::http1 as client;
 use hyper::service::service_fn;
+use tls_listener::TlsListener;
+use tls_listener::rustls::TlsAcceptor;
+use tls_listener::rustls::rustls;
 use serde::Deserialize;
 
 type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -14,6 +19,8 @@ type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
 #[derive(Deserialize)]
 struct Config {
   listen: SocketAddr,
+  cert_chain: String,
+  private_key: String,
   services: HashMap<String, SocketAddr>,
 }
 
@@ -26,35 +33,53 @@ impl Config {
 #[tokio::main]
 async fn main() -> Result {
   let config = Config::load()?;
-  let listener = TcpListener::bind(config.listen).await?;
+  let acceptor = TlsAcceptor::from(Arc::new(
+    rustls::ServerConfig::builder()
+      .with_safe_defaults()
+      .with_no_client_auth()
+      .with_single_cert(
+        rustls_pemfile::certs(&mut BufReader::new(File::open(config.cert_chain)?))?
+          .iter()
+          .map(|c| rustls::Certificate(c.clone()))
+          .collect(),
+        rustls::PrivateKey(
+          rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(File::open(config.private_key)?))?
+            [0]
+            .clone(),
+        ),
+      )?,
+  ));
+  let mut listener = TlsListener::new(acceptor, TcpListener::bind(config.listen).await?);
+
   println!("listening on {}", config.listen);
   for (host, addr) in &config.services {
     println!("{} -> {}", host, addr);
   }
   loop {
-    let (stream, _) = listener.accept().await?;
-    task::spawn({
-      let services = config.services.clone();
-      async move {
-        server::Builder::new()
-          .serve_connection(
-            stream,
-            service_fn(|req| async {
-              let stream = TcpStream::connect(
-                services
-                  .get(req.headers().get(header::HOST).unwrap().to_str().unwrap())
-                  .unwrap(),
-              )
-              .await
-              .unwrap();
-              let (mut sender, conn) = client::handshake(stream).await?;
-              task::spawn(async move { conn.await });
-              sender.send_request(req).await
-            }),
-          )
-          .await
-          .unwrap()
-      }
-    });
+    if let Some(Ok(stream)) = listener.accept().await {
+      task::spawn({
+        let services = config.services.clone();
+        async move {
+          server::Builder::new()
+            .serve_connection(
+              stream,
+              service_fn(|req| async {
+                let stream = TcpStream::connect(
+                  services
+                    .get(req.headers().get(header::HOST).unwrap().to_str().unwrap())
+                    .unwrap(),
+                )
+                .await
+                .unwrap();
+                let (mut sender, conn) = client::handshake(stream).await?;
+                task::spawn(async move { conn.await });
+                sender.send_request(req).await
+              }),
+            )
+            .await
+            .unwrap()
+        }
+      });
+    }
   }
 }
