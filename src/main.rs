6eae7ea32669847da5c3787a9a193c0f67368661
goer <georgiytnimov@gmail.com>
2024-01-31 19:05:30 +0000

    surely you jest

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ async fn main() -> Result {
     if let Some(Ok(stream)) = listener.accept().await {
       task::spawn({
         let services = config.services.clone();
-        dbg!("{:?}",&stream.peer_addr());
+        dbg!("{:?}",&stream.peer());
         async move {
           server::Builder::new()
             .serve_connection(
